bce9c0530626544c3d7eee7668cd701936de4b4a
Ethan Brooks <ethanabrooks@gmail.com>
2021-01-07 20:49:06 -0500

    back to original

--- third_restored.py
+++ third_restored.py
@@ -1,52 +1,16 @@
-#! /usr/bin/env python3.9
-
 import argparse
 import csv
-from pathlib import Path
-from typing import List
+import os
 
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument("input_dir", type=Path, help="where data comes from")
-parser.add_argument("output_dir", type=Path, help="where new files go")
-parser.add_argument("--no-wait", dest="wait", action="store_false")
-parser.add_argument("--project")
+parser.add_argument('input_dir', help='where data comes from')
+parser.add_argument('output_dir', help='where new files go')
 args = parser.parse_args()
 
-PROJECT_NAME = args.project
-if PROJECT_NAME is None:
-    PROJECT_NAME = input("Enter project name:")
-OUTPUT_DIR = Path(args.output_dir, PROJECT_NAME)
-INPUT_DIR = Path(args.input_dir, PROJECT_NAME)
-
-
-def get_permission():
-    yes = ["y", "yes"]
-    no = ["n", "no"]
-    while True:
-        response = input().lower()
-        if response in yes:
-            return True
-        if response in no:
-            return False
-        print("Please enter", "|".join(yes), "or", "|".join(no))
-
-
-for directory in [INPUT_DIR, OUTPUT_DIR]:
-    existed = directory.exists()
-    directory.mkdir(exist_ok=True)
-    if not existed:
-        print("created", directory.absolute())
-
-if args.wait:
-    input(
-        f"Press enter when sound level measurements have been placed in {INPUT_DIR.absolute()}",
-    )
-
-OUTPUT_THIRD = Path(OUTPUT_DIR, "third_octave.csv")
-OUTPUT_OCTAVE = Path(OUTPUT_DIR, "octave.csv")
-
+OUTPUT_THIRD = os.path.join(args.output_dir, 'third_octave.csv')
+OUTPUT_OCTAVE = os.path.join(args.output_dir, 'octave.csv')
 
 line_info = [
     (172, "Leq_third", 11),
@@ -56,13 +20,8 @@ line_info = [
 
 
 def preprocess(line):
-    return (
-        line.replace("\n", "")
-        .replace("Hz", "")
-        .replace("/ ", ".")
-        .replace(" ", "")
-        .split(",")
-    )
+    return line.replace("\n", "").replace('Hz', '').replace("/ ", ".").replace(
+        " ", "").split(",")
 
 
 def get_line_number(line):
@@ -72,61 +31,64 @@ def get_line_number(line):
         return None
 
 
-def octave_band(third_octave_values: List[str]) -> np.ndarray:
+def octave_band(third_octave_values):
+    def dB_add(a):
+        return 10 * np.log10(10**(float(third_octave_values[a + 0]) / 10) +
+                             10**(float(third_octave_values[a + 1]) / 10) +
+                             10**(float(third_octave_values[a + 2]) / 10))
+
     assert len(third_octave_values) % 3 == 0
-    values = np.array(third_octave_values).astype(float).reshape(-1, 3)
-    return 10 * np.log10(np.sum(10 ** (values / 10), axis=-1))
+    return [dB_add(n) for n in range(0, len(third_octave_values), 3)]
 
-
-filename = next(INPUT_DIR.iterdir())
-with filename.open(encoding="latin-1") as f:
+filename = os.listdir(args.input_dir)[0]
+with open(os.path.join(args.input_dir, filename), encoding="latin-1") as f:
     for line in f:
         if get_line_number(line) == 174:
-            _, *tail = preprocess(line)
-            third_octave_band = ["", "", "dBA", *tail]
+            third_octave_band = ['', '', "dBA"] + preprocess(line)[1:]
             break
 
 try:
-    print("Third octave band:", third_octave_band)
+    print('Third octave band:', third_octave_band)
 except NameError:
-    print("Line 174 not found in", filename, "for definition of third octave band")
+    print('Line 174 not found in', filename,
+          'for definition of third octave band')
     exit()
 
-with OUTPUT_THIRD.open("w", newline="") as third_csv, OUTPUT_OCTAVE.open(
-    "w", newline=""
-) as octave_csv:
+with open(OUTPUT_THIRD, "w", newline="") as third_csv,\
+        open(OUTPUT_OCTAVE, "w", newline="") as octave_csv:
     third_writer = csv.writer(third_csv)
     octave_writer = csv.writer(octave_csv)
 
-    head, *_ = array = np.array(third_octave_band).reshape(-1, 3)
-    octave_header = [*head, *array[1:, 1]]
+    octave_header = third_octave_band[:3]
+    octave_header += [
+        third_octave_band[i] for i in range(4, len(third_octave_band), 3)
+    ]
 
     for third_octave_line, category, dBA_line in line_info:
-        print("Generating data for", category)
+        print('Generating data for', category)
         third_writer.writerow(third_octave_band)
         octave_writer.writerow(octave_header)
 
-        for path in INPUT_DIR.iterdir():
-            print("Generating data from", path.name)
-            with path.open(encoding="latin-1") as slmdl:
+        for file_name in os.listdir(args.input_dir):
+            print('Generating data from', file_name)
+            path = os.path.join(args.input_dir, file_name)
+            with open(path, encoding="latin-1") as slmdl:
 
                 for line in slmdl:
                     line_number = get_line_number(line)
 
                     if line_number == third_octave_line:
-                        _, *third_octave_values = preprocess(line)
+                        third_octave_values = preprocess(line)[1:]
                     if line_number == dBA_line:
-                        _, dBA, *_ = preprocess(line.replace("Hz", ""))
 
-                third_octave_row = [path.name, category, dBA, *third_octave_values]
+                        dBA = preprocess(line.replace('Hz', ''))[1]
+
+                third_octave_row = [file_name, category, dBA
+                                    ] + third_octave_values
                 third_writer.writerow(third_octave_row)
-                octave_row = [
-                    path.name,
-                    category,
-                    dBA,
-                    *octave_band(third_octave_values),
-                ]
+                octave_row = [file_name, category, dBA] + \
+                    octave_band(third_octave_values)
 
                 octave_writer.writerow(octave_row)
 
-print("Files written to", OUTPUT_DIR.absolute())
+print('Files written to', os.path.realpath(args.output_dir))
